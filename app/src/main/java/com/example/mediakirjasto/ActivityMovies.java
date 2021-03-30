ff950001986c7c18a645a0eef74ddd6a7a9ec6e0
Zinzalo <sateri.sinisalo@gmail.com>
2021-03-30 13:20:01 +0300

    gfsdf

---
+++ app/src/main/java/com/example/mediakirjasto/ActivityMovies.java
@@ -0,0 +1,14 @@
+package com.example.mediakirjasto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityMovies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies);
+    }
+}
--- app/src/main/java/com/example/mediakirjasto/MainActivity.java
+++ app/src/main/java/com/example/mediakirjasto/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.mediakirjasto;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonGames = (Button) findViewById(R.id.button_games);
+        buttonGames.setOnClickListener(this);
+        Button buttonBooks = (Button) findViewById(R.id.button_books);
+        buttonBooks.setOnClickListener(this);
+        Button buttonMovies = (Button) findViewById(R.id.button_movies);
+        buttonMovies.setOnClickListener(this);
+        Button buttonMusic = (Button) findViewById(R.id.button_music);
+        buttonMusic.setOnClickListener(this);
+
         DatabaseHandler db = new DatabaseHandler(this);
 
         //Inserting games
@@ -30,7 +43,31 @@ public class MainActivity extends AppCompatActivity {
             String log = "Id: " + gm.getID() + " ,Name: " + gm.getName();
             Log.d("Name: ", log);
         }
-
-
     }
+    //user taps button
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button_books:
+                Intent intent = new Intent(this, ActivityBooks.class);
+                startActivity(intent);
+                break;
+            case R.id.button_games:
+                intent = new Intent(this, ActivityGames.class);
+                startActivity(intent);
+                break;
+            case R.id.button_movies:
+                intent = new Intent(this, ActivityMovies.class);
+                startActivity(intent);
+                break;
+            case R.id.button_music:
+                intent = new Intent(this, ActivityMusic.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+
 }
